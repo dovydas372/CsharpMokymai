4e75aa19886d2875a5ea7f9cb810857cb894ccde
Dovydas Gailiūnas <108748876+dovydas372@users.noreply.github.com>
2026-03-28 10:46:28 +0200

    padarytas prekes api ir modelis

---
+++ paskaita-11-praktika-API-part2/Controllers/PrekesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using paskaita_11_praktika_API_part2.Models;
+
+namespace paskaita_11_praktika_API_part2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrekesController : ControllerBase
+    {
+
+        public static List<Preke> PrekiuSarasas { get; set; } = new List<Preke>();
+
+        [HttpPost()]
+        public IActionResult SukurtiPreke(Preke naujaPreke)
+        {
+            try
+            {
+                if (naujaPreke == null)
+                {
+                    return BadRequest("Neatsiųsti prekės duomenys.");
+                }
+                naujaPreke.Id = PrekiuSarasas.Count + 1;
+                PrekiuSarasas.Add(naujaPreke);
+                return Created($"/api/Prekes/{naujaPreke.Id}", naujaPreke);
+
+            }
+            catch (Exception ex)
+            {
+                return Problem(
+                    detail: $"{ex}",
+                    statusCode: 500,
+                    title: "Serverio klaida",
+                    type: "https://httpstatuses.com/500"
+                    );
+            }
+        }
+    }
+}

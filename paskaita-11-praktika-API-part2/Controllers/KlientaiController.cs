0be1596702e697eec8a448069b99d43c214c0c7b
Dovydas Gailiūnas <108748876+dovydas372@users.noreply.github.com>
2026-04-01 19:21:15 +0300

    Klientai Controller ir Klientas Model

---
+++ paskaita-11-praktika-API-part2/Controllers/KlientaiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using paskaita_11_praktika_API_part2.Models;
+
+namespace paskaita_11_praktika_API_part2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KlientaiController : ControllerBase
+    {
+        public List<Klientas> Klientai { get; set; } = new List<Klientas>();  
+
+        public IActionResult SukurtiKlienta(Klientas klientas)
+        {
+            try
+            {
+                klientas.Id = Klientai.Count + 1;
+
+                Klientai.Add(klientas);
+
+                return Created($"/api/Klientai/{klientas.Id}", klientas);
+
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
